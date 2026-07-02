ad2b1a673af3143387f7190c376fa74442904c69
Nandang Eka Prasetya <72372613+naneps@users.noreply.github.com>
2026-07-02 07:45:39 +0700

    feat: wire React app entry

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Supra Airways landing page');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+import './styles.css';
+
+const mount = document.querySelector('#root');
+createRoot(mount).render(React.createElement(App));
